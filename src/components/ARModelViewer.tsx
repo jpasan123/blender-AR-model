ce7d7d521e5022c586cfff3814c827fbff349d6c
Janidu Pasan Hewapathirana <138660521+jpasan123@users.noreply.github.com>
2024-12-12 15:56:20 +0530

    update-commit

--- src/components/ARModelViewer.tsx
+++ src/components/ARModelViewer.tsx
@@ -12,7 +12,7 @@ const ARModelViewer: React.FC = () => {
     // Navigate to thank you page after 5 seconds
     const timer = setTimeout(() => {
       navigate('/thank-you');
-    }, 12000);
+    }, 15000);
 
     return () => clearTimeout(timer);
   }, [navigate]);
